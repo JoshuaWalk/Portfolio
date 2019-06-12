202cf0bc8e97ffc469602b22c39968ea3e2d5e77
Joshua Walker <joshuassignup@gmail.com>
2019-06-12 08:49:29 -0600

    maybe fixed routing issue, removed old pictures

--- src/Route.js
+++ src/Route.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 import Info from './components/info/Info'
-import Me from './components/Me'
 import Projects from './components/Projects'
 import AboutMe from './components/AboutMe'
 
@@ -10,8 +9,7 @@ const Routing = () => (
 
     <Router>
         <Switch>
-            <Route exact path = "/" component={Me}/>
-            <Route exact path = "/about" component={AboutMe}/>
+            <Route exact path = "/" component={AboutMe}/>
             <Route path = "/projects" component={Projects}/>
             <Route path = "/info" component= {Info}/>
         </Switch>
